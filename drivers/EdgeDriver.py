a1db1286feeae8adb44f5b3965b81b42815b7e8d
VinhTongThanh <vinhtongthanh57@gmail.com>
2023-05-09 14:40:00 +0700

    update driver and main.py

--- drivers/EdgeDriver.py
+++ drivers/EdgeDriver.py
@@ -3,20 +3,18 @@ from selenium.webdriver.edge.service import Service
 
 from notification import GeneralNotification
 
-"""Initializes Edge driver"""
-
-def initiate():    
-    edge_options = webdriver.EdgeOptions()
-    edge_options.add_argument('--disable-gpu')
-    edge_options.add_argument('--headless')
-    edge_options.add_argument('--no-sandbox')
-    edge_options.add_argument('--disable-dev-shm-usage')
-    edge_service = Service(executable_path='edgedriver')
-
-    driver = webdriver.Edge(service=edge_service, options=edge_options)
-    if driver is None:
-        GeneralNotification.notify("Create Edge Driver", "failure")
-    else:
+def initiate():
+    try:
+        edge_options = webdriver.EdgeOptions()
+        edge_options.add_argument('--disable-gpu')
+        edge_options.add_argument('--headless')
+        edge_options.add_argument('--no-sandbox')
+        edge_options.add_argument('--disable-dev-shm-usage')
+        edge_service = Service(executable_path='edgedriver')
+        driver = webdriver.Edge(service=edge_service, options=edge_options)
         GeneralNotification.notify("Create Edge Driver", "success")
-
-    return driver
+        return driver
+    except Exception as e:
+        GeneralNotification.notify("Create Edge Driver", "failure")
+        print("Error creating Edge driver: ", e)
+        return None
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import time
 
+from drivers import EdgeDriver
 from models import WineScraperModel
 from tools import CreateFolder
 
@@ -14,33 +15,27 @@ wines = []
 wine_infos = pd.DataFrame(
     columns=['appelation', 'producer', 'region', 'country', 'vintage'
              'wine_type', 'grape_variety', 'alcohol_content', 'volume'])
-time_out = 3
+max_page_number = 50
 
-# # Spain Wines
+driver = EdgeDriver.initiate()
 
-# # 
-# wineScraperModel = WineScraperModel.WineScraper()
-# spain_wine_names, spain_wine_urls = wineScraperModel.scrape_wine_cards(driver, urls[0], n=15)
-# len(spain_wine_names), len(spain_wine_urls)
+# 
+wineScraperModel = WineScraperModel.WineScraper()
+wine_names, wine_urls = wineScraperModel.scrape_wine_cards(driver, urls[0], n=max_page_number)
 
-# # 
-# cols = ['appelation', 'producer', 'region', 'country', 'vintage', 'wine_type', 'grape_variety', 'alcohol_content', 'volume', 'image_filename']
-# df = pd.DataFrame(columns=cols)
-# saved_folder = CreateFolder.create_new_folder("image")
-# df.head()
+# 
+cols = ['appelation', 'producer', 'region', 'country', 'vintage', 'wine_type', 'grape_variety', 'alcohol_content', 'volume', 'image_filename']
+df = pd.DataFrame(columns=cols)
+saved_folder = CreateFolder.create_new_folder("image")
 
-# # 
-# # insert values into the dataframe
-# for i in range(0, len(spain_wine_urls)):
-#     temp = wineScraperModel.scrape_wine_info(driver, spain_wine_urls[i], spain_wine_names[i], saved_folder)
-#     if temp[0] != -1:
-#         df.loc[i] = temp
-#     time.sleep(5)
-#     print(f'Finished looking at item {spain_wine_names[i]}, need some sleep\n...\nOk, Back to Work')
+# insert values into the dataframe
+for i in range(0, len(wine_urls)):
+    temp = wineScraperModel.scrape_wine_info(driver, wine_urls[i], wine_names[i], saved_folder)
+    if temp[0] != -1:
+        df.loc[i] = temp
+    time.sleep(5)
+    print(f'Finished looking at item {wine_names[i]}, need some sleep\n...\nOk, Back to Work')
 
-# # 
-# CreateFolder.create_new_folder("wine_data")
-# df.to_csv('wine_data/spain_wine_data.csv')
-
-# # 
-# df.head()
+# 
+CreateFolder.create_new_folder("wine_data")
+df.to_csv('wine_data/wine_data.csv')
